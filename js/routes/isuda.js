4fe2b8ef9cfd74190a77ebf69dd001a55e032281
tabatak <hamamoto.kazuto@gmail.com>
2017-09-19 22:42:32 +0900

    reset時にはhtmlifyしない

--- js/routes/isuda.js
+++ js/routes/isuda.js
@@ -415,8 +415,8 @@ const resetCachedHtmlified = async (ctx, keyword) => {
   const db = await dbh(ctx);
   const entries = await db.query("SELECT id, description FROM entry where keyword <> '?' AND description LIKE '%?%'", [keyword, keyword])
   for (let entry of entries) {
-    const htmlified = await htmlify(ctx, entry.description);
-    await redisClient.setAsync(`htmlified-${entry.id}`, htmlified);
+    // const htmlified = await htmlify(ctx, entry.description);
+    await redisClient.del(`htmlified-${entry.id}`);
   }
 }
 
